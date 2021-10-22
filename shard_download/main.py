e1ad9bf2e143335f0bdefc937d054be427e8e347
WindSnowLi <61129607+WindSnowLi@users.noreply.github.com>
2021-10-22 17:10:46 +0800

    fix  ’path ->‘ 和 ’分片下载‘

--- shard_download/main.py
+++ shard_download/main.py
@@ -6,15 +6,6 @@ import requests
 # 设置超时时间为30s
 # socket.setdefaulttimeout(30)
 
-# 默认每个片最小2MB
-DEFAULT_MIN_PART = 2048 * 1024
-
-# 每个组分配下载量大小
-GROUP_PART = 0
-
-# 最大进程<=10
-MAX_THREAD = 10
-
 
 def make_file_buff(name, length):
     """
@@ -27,41 +18,58 @@ def make_file_buff(name, length):
         f.write(b'\x00')
 
 
-def write_part(name, index, part):
+def write_part(name, index, part, config):
     """
     写入片
     :param name: 文件名
     :param index: 第几个片
     :param part: 二进制片
+    :param config: 整体配置
     """
     with open(name, 'rb+') as f:
-        f.seek(index * GROUP_PART + DEFAULT_MIN_PART)
+        f.seek(index * config['GROUP_PART'] + config['DEFAULT_MIN_PART'])
         f.write(part)
 
 
-def down_part(url, name, index):
+def down_part(url, name, index, config):
     """
     下载片
     :param url: url链接
     :param name: 文件名
     :param index: 下载第几个片
+    :param config: 整体配置
     :return: 片的索引
     """
     while True:
         try:
+            print('片{0}开始下载'.format(index))
             write_part(name, index, requests.get(url, headers={'Range': 'bytes=%d-%d' % (
-                index * GROUP_PART + DEFAULT_MIN_PART, (index + 1) * GROUP_PART + DEFAULT_MIN_PART)}).content)
+                index * config['GROUP_PART'] + config['DEFAULT_MIN_PART'], (index + 1) * config['GROUP_PART'] + config['DEFAULT_MIN_PART'])}).content, config)
             return index
         except Exception as e:
             print(e)
 
 
-if __name__ == '__main__':
-    url = 'https://download.dbeaver.com/community/21.2.2/dbeaver-ce_21.2.2_amd64.deb'
+def main():
+    config = {
+
+        # 默认每个片最小2MB
+        'DEFAULT_MIN_PART': 2048 * 1024,
+
+        # 每个组分配下载量大小
+        'GROUP_PART': 0,
+
+        # 最大进程<=10
+        'MAX_THREAD': 10
+    }
+
+    url = 'https://dbeaver.io/files/dbeaver-ce-latest-x86_64-setup.exe'
     # 解析文件名
     file_name = url[url.rindex('/') + 1:]
     # 默认下载2M的开头
-    res = requests.get(url, headers={'Range': 'bytes=0-%d' % DEFAULT_MIN_PART})
+
+    res = requests.get(
+        url, headers={'Range': 'bytes=0-%d' % config['DEFAULT_MIN_PART']})
     # 根据返回的头信息解析总长度
     content_length = res.headers['Content-Range']
     # 总长度
@@ -72,28 +80,35 @@ if __name__ == '__main__':
         f.write(res.content)
 
     # 如果总长度小于最小片，则已经下载完成
-    if size <= DEFAULT_MIN_PART:
+    if size <= config['DEFAULT_MIN_PART']:
         exit(0)
     # 以最小片计算所需线程数
-    workers = math.ceil(size / DEFAULT_MIN_PART) - 1
+    workers = math.ceil(size / config['DEFAULT_MIN_PART']) - 1
     # 大于10则限定在10以内
     if workers >= 10:
-        GROUP_PART = math.ceil((size - DEFAULT_MIN_PART) / 10)
+        config['GROUP_PART'] = math.ceil(
+            (size - config['DEFAULT_MIN_PART']) / 10)
     else:
-        GROUP_PART = math.ceil((size - DEFAULT_MIN_PART) / DEFAULT_MIN_PART)
+        config['GROUP_PART'] = math.ceil(
+            (size - config['DEFAULT_MIN_PART']) / config['DEFAULT_MIN_PART'])
 
     # 最终实际线程数
-    workers = math.ceil((size - DEFAULT_MIN_PART) / GROUP_PART)
+    workers = math.ceil(
+        (size - config['DEFAULT_MIN_PART']) / config['GROUP_PART'])
 
     # 最多10个进程同时下载
-    processPool = ProcessPoolExecutor(max_workers=MAX_THREAD)
+    processPool = ProcessPoolExecutor(max_workers=config['MAX_THREAD'])
 
     futures = {}
     for i in range(0, workers):
         # 提交进程下载
-        job = processPool.submit(down_part, url, file_name, i)
+        job = processPool.submit(down_part, url, file_name, i, config)
         futures[job] = i
     for job in futures:
         re = job.result()
         if re != -1:
             print('片{0}下载结束'.format(re))
+
+
+if __name__ == "__main__":
+    main()
